b5ca5cbed3930a2d119d05aea8b528d68bfdbf8d
erimsalih2 <sevilmiserim@gmail.com>
2025-02-11 16:36:06 +0300

    Proje dosyası ekle.

---
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using JwtAuthDotNet9.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService service) : ControllerBase
+    {
+        public static User user = new User();
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var result = await service.RegisterAsync(request);
+            if(result is not null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            var result = await service.LoginAsync(request);
+            if (result is not null)
+            {
+                return Ok(result);
+            }
+            
+            return BadRequest();
+
+        }
+        
+
+    }
+}
---
+++ JwtAuthDotNet9/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+
+namespace JwtAuthDotNet9.Services
+{
+    public interface IAuthService
+    {
+        Task<User?>  RegisterAsync(UserDto userDto);
+        Task<string?> LoginAsync(UserDto userDto);
+    }
+}
